8a4b2670fb5c7b3d82d99c510a20f05b302a1e39
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-09-08 08:03:39 +0200

    :recycle: Correctly reproduce the issue

--- packages/package-three/src/index.ts
+++ packages/package-three/src/index.ts
@@ -1,8 +1,8 @@
 import { schema } from "package-two";
 
-const yos = [
+const yos = schema.parse([
   { id: "1", name: "yo" },
   { id: "2", name: "yo" },
-].map((item) => schema.parse(item));
+]);
 
 yos.map((yo) => yo.id);
